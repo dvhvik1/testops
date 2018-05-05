55542f29ec4a0fccd55fb1f90b0051b63a9bc11a
vikclass <vikclass@gmail.com>
2018-05-06 05:34:27 +0700

    Some commit message

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var Config = {
   "domain":     process.env.TESTOPS_SERVICE_HOST || '127.0.0.1',
 
   "serverip":   process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  "serverport": process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  "serverport": process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80,
   
-  "clientport": (process.env.TESTOPS_SERVICE_PORT) ? 8000:8080,
+  "clientport":  80:8080,
   "protocol":   'ws://',
 
   "heartbeattmo": 1000, // milliseconds 
