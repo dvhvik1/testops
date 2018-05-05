82be06025a4ee9ec62b428658a6168dda6d0a01a
vikclass <vikclass@gmail.com>
2018-05-06 04:02:13 +0700

    Some commit message

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 console.log(process.env);
 console.log("Trying to start server with config:", config.serverip + ":" + config.serverport);
-config.serverport=process.env.TESTOPS_SERVICE_HOST || '127.0.0.1';
-config.serverip=process.env.TESTOPS_SERVICE_PORT || '8080';
+config.serverip=process.env.TESTOPS_SERVICE_HOST || '127.0.0.1';
+config.serverport=process.env.TESTOPS_SERVICE_PORT || '8080';
 // Both port and ip are needed for the OpenShift, otherwise it tries 
 // to bind server on IP 0.0.0.0 (or something) and fails
 server.listen(config.serverport, config.serverip, function() {
