aa7adc059b5ddd8ab31fcaf0206c51ab87e6e91d
vikclass <vikclass@gmail.com>
2018-05-06 03:18:35 +0700

    Some commit message

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var Config = {
   // Contents of this file will be send to the client
+  console.log(spec);
+  console.log(process);
   "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
 
   "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
