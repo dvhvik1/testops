46560c39a105de017031ea630f35cd4b5cad61b1
vikclass <vikclass@gmail.com>
2018-05-06 03:48:53 +0700

    Some commit message

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var Config = {
   // Contents of this file will be send to the client
-  "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
+  "domain":     process.env.TESTOPS_SERVICE_HOST || '127.0.0.1',
 
-  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
+  "serverip":   process.env.TESTOPS_SERVICE_HOST || '127.0.0.1',
+  "serverport": process.env.TESTOPS_SERVICE_PORT || '8080',
   
-  "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
+  "clientport": (process.env.TESTOPS_SERVICE_PORT) ? '8000':'8080',
   "protocol":   'ws://',
 
   "heartbeattmo": 1000, // milliseconds 
